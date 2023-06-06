63ffa3b5ff6ee0af5548fded59f3ec303189efd5
jhlim <61223379+imjh8751@users.noreply.github.com>
2023-06-06 21:50:07 +0900

    오픈 소스, 자체 호스팅 프로젝트 계획 도구 Plane

--- docker/plane/install.sh
+++ docker/plane/install.sh
@@ -1,4 +1,18 @@
-curl https://raw.githubusercontent.com/makeplane/plane/develop/docker-compose-hub.yml --output docker-compose.yml
-curl https://raw.githubusercontent.com/makeplane/plane/develop/setup.sh --output setup.sh
+#Docker Compose Setup
+#Clone the repository
+git clone https://github.com/makeplane/plane
+cd plane
 chmod +x setup.sh
-./setup.sh localhost
+
+#Run setup.sh
+./setup.sh http://localhost 
+
+#If running in a cloud env replace localhost with public facing IP address of the VM
+
+#Export Environment Variables
+set -a
+source .env
+set +a
+
+#Run Docker compose up
+docker compose up -d
