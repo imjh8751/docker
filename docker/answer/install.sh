f71fabe91870e8edf6fe434ba7a1cb51b71d1eaa
jhlim <61223379+imjh8751@users.noreply.github.com>
2023-09-04 12:57:14 +0900

    Q&A 서비스

--- docker/answer/install.sh
+++ docker/answer/install.sh
@@ -1,1 +1,4 @@
+# https://answer.dev/docs/installation
+# https://github.com/answerdev/answer
+
 curl -fsSL https://raw.githubusercontent.com/answerdev/answer/main/docker-compose.yaml | docker-compose -p answer -f - up
