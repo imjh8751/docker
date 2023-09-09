9ffcaaa95ffdd5d86bdecaf94d15aef76da986f4
jhlim <61223379+imjh8751@users.noreply.github.com>
2023-09-09 23:00:43 +0900

    Create install_kubesphere.sh

---
+++ k8s/kubesphere/install_kubesphere.sh
@@ -0,0 +1,3 @@
+curl -sfL https://get-kk.kubesphere.io | VERSION=v3.0.7 sh -
+./kk create cluster --with-kubernetes v1.22.17 --with-kubesphere v3.4.0
+kubectl logs -n kubesphere-system $(kubectl get pod -n kubesphere-system -l 'app in (ks-install, ks-installer)' -o jsonpath='{.items[0].metadata.name}') -f
