a2ea76901d42a43747539c0a84e0e55440c25bc3
jhlim <61223379+imjh8751@users.noreply.github.com>
2023-12-10 21:58:37 +0900

    Update install_ubuntu_package.sh

--- install_ubuntu_package.sh
+++ install_ubuntu_package.sh
@@ -2,7 +2,7 @@
 sudo apt update -y && sudo apt upgrade -y
 
 # install package
-sudo apt install net-tools curl vim git samba openssh-server -y
+sudo apt install net-tools curl vim git samba openssh-server python3-pip -y
 
 # ssh key generate
 ssh-keygen -t rsa
