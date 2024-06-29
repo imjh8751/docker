45f4599d8ab56dca1578558989835a5f8fe2b85d
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-06-29 21:32:25 +0900

    Update mount-service.sh

--- mount-service.sh
+++ mount-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 도커 실행 권한 부여
-chmod 750 /root/docker/docker/mount-docker.sh
+chmod 750 /root/docker/docker/*.sh
 
 # /etc/systemd/system/mount-docker.service 파일 생성
 cat <<EOF > /etc/systemd/system/mount-docker.service
