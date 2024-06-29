812a99dbbe8097eb9a9e0c87af0075b4affe0297
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-06-29 21:30:41 +0900

    Create mount-service.sh

---
+++ mount-service.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# 도커 실행 권한 부여
+chmod 750 /root/docker/docker/mount-docker.sh
+
+# /etc/systemd/system/mount-docker.service 파일 생성
+cat <<EOF > /etc/systemd/system/mount-docker.service
+[Unit]
+Description=Mount Script Service
+After=network.target
+
+[Service]
+Type=oneshot
+ExecStart=/root/docker/docker/docker-mount.sh
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# systemctl daemon-reload, enable, start 추가 작성
+sudo systemctl daemon-reload
+sudo systemctl enable mount-docker.service
+sudo systemctl start mount-docker.service
+
+# 도커 실행
+bash /root/docker/docker/mount-docker.sh
