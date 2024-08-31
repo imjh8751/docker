29d99ec29019eecd5d4488a118083104f61f726b
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-08-31 19:55:43 +0900

    Create create-user.sh

---
+++ docker/OFFICE/paperless-ngx/create-user.sh
@@ -0,0 +1,7 @@
+root@dev-document:~/docker/docker/OFFICE/paperless-ngx# docker exec -it paperless-ngx-webserver-1 bash
+root@cfafc068e089:/usr/src/paperless/src# python3 manage.py createsuperuser
+Username (leave blank to use 'root'): admin
+Email address: admin@gmail.com
+Password: 
+Password (again): 
+Superuser created successfully.
