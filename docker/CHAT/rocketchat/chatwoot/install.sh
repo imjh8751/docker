8f76d1d4f762d0deb5ac10a14e4ddd52eed37a16
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-10-04 08:58:57 +0900

    Create install.sh

---
+++ docker/CHAT/rocketchat/chatwoot/install.sh
@@ -0,0 +1,4 @@
+# Download the env file template
+wget -O .env https://raw.githubusercontent.com/chatwoot/chatwoot/develop/.env.example
+# Download the Docker compose template
+wget -O docker-compose.yaml https://raw.githubusercontent.com/chatwoot/chatwoot/develop/docker-compose.production.yaml
