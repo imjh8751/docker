f1d55dd3b79e3b6646532785088b8b05e954901d
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-07 21:21:05 +0900

    Create 11.set_localtime.sh

---
+++ openshift/11.set_localtime.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 서버 목록
+servers=("192.168.0.70" "192.168.0.71" "192.168.0.72" "192.168.0.73" "192.168.0.74" "192.168.0.75")
+
+# 명령어 실행 함수
+execute_command() {
+    local server=$1
+    ssh core@$server "sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime"
+}
+
+# 각 서버에 명령어 실행
+for server in "${servers[@]}"; do
+    echo "Executing on $server..."
+    execute_command $server
+    echo "Done with $server"
+done
+
+echo "All commands executed successfully."
+
+# install bootstrap and master
+openshift-install --dir installation_directory wait-for bootstrap-complete --log-level=info
