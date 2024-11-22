eaf06c31220226659dee93498999d6cdc9d8bf77
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-22 15:52:08 +0900

    Update 7.add_nginx.conf.sh

--- openshift/7.add_nginx.conf.sh
+++ openshift/7.add_nginx.conf.sh
@@ -22,10 +22,16 @@ server {
 }
 EOF
 
+# nginx.conf 파일 경로 설정
+CONFIG_FILE="/etc/nginx/nginx.conf"
+
+# 80번 포트를 사용하는 server 블록 주석 처리
+sudo sed -i '/server {/,/}/ {/listen 80;/,/}/ s/^/#/' "$CONFIG_FILE"
+
 echo "Server configuration has been added to $NGINX_CONF"
 
 # 폴더 생성
-mkdir /usr/share/nginx/html/files
+mkdir -p /usr/share/nginx/html/files
 
 # 방화벽 중지
 systemctl stop firewalld
