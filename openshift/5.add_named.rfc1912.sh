2eaaf6b70fea2c4e93e4895f034c6af492d05f52
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-22 15:48:53 +0900

    Update 5.add_named.rfc1912.sh

--- openshift/5.add_named.rfc1912.sh
+++ openshift/5.add_named.rfc1912.sh
@@ -6,17 +6,17 @@ cat <<EOF >> $ZONE_CONFIG_FILE
 
 zone "okd.io" IN {
         type master;
-        #file "okd.io.zone";
-        file "okd.io.min.zone";
-        #file "okd.io.sno.zone";
+        #file "okd.io.zone";  # MASTER 3, WORKER 2, BOOTSTRAP 1, BASTION 1
+        file "okd.io.min.zone";  # MASTER 1, WORKER 2, BOOTSTRAP 1, BASTION 1
+        #file "okd.io.sno.zone";  # MASTER 3, WORKER 2, BOOTSTRAP 1, BASTION 1
         allow-update { none; };
 };
 
 zone "0.168.192.in-addr.arpa" IN {
         type master;
-        #file "0.168.192.rev.zone";
-        file "0.168.192.rev.min.zone";
-        #file "0.168.192.rev.sno.zone";
+        #file "0.168.192.rev.zone";  # MASTER 3, WORKER 2, BOOTSTRAP 1, BASTION 1
+        file "0.168.192.rev.min.zone";  # MASTER 1, WORKER 2, BOOTSTRAP 1, BASTION 1
+        #file "0.168.192.rev.sno.zone";  # MASTER 1, BOOTSTRAP 1, BASTION 1
         allow-update { none; };
 };
 EOF
