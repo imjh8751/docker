751323a825804d75b320c8a17b8051ea3443bb3f
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-22 14:53:13 +0900

    Update 2.set_zone1.sh

--- openshift/2.set_zone1.sh
+++ openshift/2.set_zone1.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# bastion(DNS+Bastion) 1개, master 3개, worker 2개, bootstrap 1개
 ZONE_FILE="/var/named/okd.io.zone"
+
+# bastion(DNS+Bastion) 1개, master 1개, worker 2개, bootstrap 1개
+ZONE_FILE2="/var/named/okd.io.zone"
+
+# bastion(DNS+Bastion) 1개, master 1개,bootstrap 1개 (SINGLE NODE용 - SNO)
+ZONE_FILE3="/var/named/okd.io.zone"
 
 cat <<EOF >> $ZONE_FILE
 \$TTL 60
@@ -28,6 +35,55 @@ master03.ocp4    IN      A        192.168.0.73
 
 worker01.ocp4    IN      A        192.168.0.74
 worker02.ocp4    IN      A        192.168.0.75
+EOF
+
+cat <<EOF >> $ZONE_FILE2
+\$TTL 60
+@ IN    SOA    ocp.ocp4.okd.io.    root (
+                            2023012001    ; serial
+                            1D       ; refresh
+                            1H       ; retry
+                            1W      ; expire
+                            3H )    ; minimum
+
+                 IN      NS      ocp.ocp4.okd.io.
+
+ocp4             IN      A        192.168.0.69
+ocp.ocp4         IN      A        192.168.0.69
+helper.ocp4      IN      A        192.168.0.69
+api.ocp4         IN      A        192.168.0.69
+api-int.ocp4     IN      A        192.168.0.69
+*.ocp4           IN      A        192.168.0.69
+
+bootstrap.ocp4   IN      A        192.168.0.70
+
+master01.ocp4    IN      A        192.168.0.71
+
+worker01.ocp4    IN      A        192.168.0.72
+worker02.ocp4    IN      A        192.168.0.73
+EOF
+
+cat <<EOF >> $ZONE_FILE3
+\$TTL 60
+@ IN    SOA    ocp.ocp4.okd.io.    root (
+                            2023012001    ; serial
+                            1D       ; refresh
+                            1H       ; retry
+                            1W      ; expire
+                            3H )    ; minimum
+
+                 IN      NS      ocp.ocp4.okd.io.
+
+ocp4             IN      A        192.168.0.69
+ocp.ocp4         IN      A        192.168.0.69
+helper.ocp4      IN      A        192.168.0.69
+api.ocp4         IN      A        192.168.0.69
+api-int.ocp4     IN      A        192.168.0.69
+*.ocp4           IN      A        192.168.0.69
+
+bootstrap.ocp4   IN      A        192.168.0.70
+
+master01.ocp4    IN      A        192.168.0.71
 EOF
 
 echo "DNS records have been added to $ZONE_FILE"
