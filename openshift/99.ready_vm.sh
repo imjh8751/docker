f228f5ea2feb60ff570e6c595ff93005ac959f8e
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-23 22:09:51 +0900

    Update and rename 99.create_vm.sh to 99.ready_vm.sh

--- openshift/99.ready_vm.sh
+++ openshift/99.ready_vm.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-#Go to https://getfedora.org/en/coreos/download?tab=metal_virtualized&stream=stable, replace version
 # 1. Make ISO Download
-VERSION=32.20201104.3.0
-
-if [[ -f "fedora-coreos.qcow2" ]]; then
-    rm fedora-coreos.qcow2
+if [[ -f "rhcos-live.iso" ]]; then
+    rm rhcos-live.iso
 fi
 
-wget https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/$VERSION/x86_64/fedora-coreos-$VERSION-qemu.x86_64.qcow2.xz
-unxz fedora-coreos-$VERSION-qemu.x86_64.qcow2.xz
-mv fedora-coreos-$VERSION-qemu.x86_64.qcow2 fedora-coreos.qcow2
+# coreos 파일 다운로드
+openshift-install coreos print-stream-json | grep '.iso[^.]' | grep x86_64
+wget -O rhcos-live.iso https://rhcos.mirror.openshift.com/art/storage/prod/streams/4.17-9.4/builds/417.94.202409120353-0/x86_64/rhcos-417.94.202409120353-0-live.x86_64.iso
 
 # 2. Make VM Template
 ID=29999
-NAME=fcos-template
+NAME=rhcos-template
 STORAGE=nvme
 CPU=4
 RAM=16384
@@ -24,7 +21,7 @@ VLAN=20
 ADD_STORAGE=112G
 
 /usr/sbin/qm create $ID --name $NAME --cores $CPU --memory $RAM --net0 $NET_TYPE,bridge=$BRIDGE,tag=$VLAN
-/usr/sbin/qm importdisk $ID fedora-coreos.qcow2 $STORAGE
+/usr/sbin/qm importdisk $ID rhcos-live.iso $STORAGE
 /usr/sbin/qm set $ID --scsihw virtio-scsi-pci --scsi0 $STORAGE:vm-$ID-disk-0
 /usr/sbin/qm set $ID --boot c --bootdisk scsi0
 /usr/sbin/qm resize $ID scsi0 +$ADD_STORAGE
