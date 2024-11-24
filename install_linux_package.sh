85d18889d76c2f2adef3250046de68b33c9cb564
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-24 22:20:19 +0900

    Update install_linux_package.sh

--- install_linux_package.sh
+++ install_linux_package.sh
@@ -2,7 +2,19 @@
 sudo yum update -y && sudo apt upgrade -y
 
 # install package
-sudo yum install net-tools curl vim git samba openssh-server python3-pip wget tar rsync nc podman cloud-init -y
+sudo yum install net-tools curl vim git samba openssh-server python3-pip wget tar rsync nc podman cloud-init tcpdump psmisc bind-utils epel-release qemu-guest-agent -y
+
+# firewalld disable
+systemctl disable firewalld
+
+# qemu-guest-agent up
+systemctl enable qemu-guest-agent
+ 
+# selinux disable
+# vim /etc/selinux/config
+# SELINUX=disabled
+
+yum update -y
 
 # ssh key generate
 ssh-keygen -t rsa
