9d6e603d238cde68d777c278a01f76a9cf24f8bc
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-24 21:59:32 +0900

    Update install_linux_package.sh

--- install_linux_package.sh
+++ install_linux_package.sh
@@ -2,7 +2,7 @@
 sudo yum update -y && sudo apt upgrade -y
 
 # install package
-sudo yum install net-tools curl vim git samba openssh-server python3-pip nfs-common wget tar rsync nc podman -y
+sudo yum install net-tools curl vim git samba openssh-server python3-pip wget tar rsync nc podman -y
 
 # ssh key generate
 ssh-keygen -t rsa
