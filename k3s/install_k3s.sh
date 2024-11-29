3bf622e2d7d2cdaf2843f4760a8d17a5148e0480
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-29 12:49:32 +0900

    Update install_k3s.sh

--- k3s/install_k3s.sh
+++ k3s/install_k3s.sh
@@ -23,19 +23,8 @@ source ~/.bashrc
 
 ##############################################
 ### 3. kubectl 설치
-# 1 Kubernetes repository 추가
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-
-# 2. kubectl 설치
-sudo yum install -y kubectl
+# 1 설치
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
 # 3. 버전확인
 kubectl version --client
