639ec7e1901d21985db04fbd28fe6bbc5d6192b0
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-30 21:15:36 +0900

    Update install_k3s.sh

--- k3s/install_k3s.sh
+++ k3s/install_k3s.sh
@@ -5,10 +5,10 @@
 #sudo apt-get install -y curl
 
 # 2. **k3s 설치**
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io  | INSTALL_K3S_EXEC="--disable=traefik" sh -
 
 # 3. **설치 확인**
-sudo systemctl status k3s
+#sudo systemctl status k3s
 
 ##############################################
 ### 2. kubectl 설정
@@ -23,11 +23,20 @@ source ~/.bashrc
 
 ##############################################
 ### 3. kubectl 설치
-# 1 설치
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# 1 k3s 복사
+sudo cp /usr/local/bin/k3s /usr/bin/
+sudo cp /usr/local/bin/kubectl /usr/bin/
 
 # 3. 버전확인
 kubectl version --client
+
+##############################################
+# kubectl 자동 완성 활성화
+source <(kubectl completion bash)
+alias k='kubectl'
+
+# bash 자동 완성
+complete -o default -F __start_kubectl k
 
 ##############################################
 ### 4. 노드 및 파드 확인
@@ -40,4 +49,4 @@ kubectl get pods --all-namespaces
 ##############################################
 ### 자5. 토큰 저장
 # 1. 토큰 저장 
-curl --upload-file /var/lib/rancher/k3s/server/node-token http://192.168.0.69:8080/token.log
+# curl --upload-file /var/lib/rancher/k3s/server/node-token http://192.168.0.69:8080/token.log
