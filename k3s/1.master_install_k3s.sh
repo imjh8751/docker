05ad0c8308c4f679395dfb2d7c6ddadcf31f8cff
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-30 22:52:16 +0900

    Update 1.master_install_k3s.sh

--- k3s/1.master_install_k3s.sh
+++ k3s/1.master_install_k3s.sh
@@ -61,4 +61,4 @@ k get pods --all-namespaces
 ### 자5. 토큰 저장
 # 1. 토큰 저장
 cat /var/lib/rancher/k3s/server/node-token
-# curl --upload-file /var/lib/rancher/k3s/server/node-token http://192.168.0.69:8080/token.log
+curl -v -u 'master:Admin2580!' -T /var/lib/rancher/k3s/server/node-token http://192.168.0.10:5005/WEBDAV/token.log
