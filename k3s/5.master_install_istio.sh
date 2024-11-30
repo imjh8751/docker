22f757c4a54e2be3a68bee936dcef74a6f11bcaa
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-30 23:28:18 +0900

    Update 5.master_install_istio.sh

--- k3s/5.master_install_istio.sh
+++ k3s/5.master_install_istio.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# istio 다운로드
+curl -L https://istio.io/downloadIstio | sh -
+
+# istioctl 복
+cd istio*
+cp ./bin/istioctl /usr/local/bin/
+
+# demo 버전 설치
+istioctl install --set profile=demo -y
+
+# default namespace injection 추가
+kubectl label namespace default istio-injection=enabled
