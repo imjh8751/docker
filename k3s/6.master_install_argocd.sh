2b40ab9c9b3182c47d6413741f44ca55278f1bb6
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-11-30 23:37:06 +0900

    Update 6.master_install_argocd.sh

--- k3s/6.master_install_argocd.sh
+++ k3s/6.master_install_argocd.sh
@@ -12,12 +12,8 @@ curl -o install.yaml https://raw.githubusercontent.com/argoproj/argo-cd/stable/m
 # --insecure 옵션 추가
 sed -i '/- \/usr\/local\/bin\/argocd-server/a\        - --insecure' install.yaml
 
-# 변경된 파일 내용 출력 (선택 사항)
-echo "변경된 install.yaml 파일 내용:"
-grep -A 3 '- \/usr\/local\/bin\/argocd-server' install.yaml
-
 # Argo CD 설치
-kubectl apply -f install.yaml
+kubectl apply -n argocd -f install.yaml
 
 #############################################################################################################
 # 참고용
