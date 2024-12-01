ed1637739a0e3dd60e569c2b53d0d926f29ad526
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-12-01 19:40:47 +0900

    Update 1.master_install_k3s.sh

--- k3s/singnode/1.master_install_k3s.sh
+++ k3s/singnode/1.master_install_k3s.sh
@@ -58,7 +58,16 @@ k get nodes
 k get pods --all-namespaces
 
 ##############################################
-### 자5. 토큰 저장
-# 1. 토큰 저장
-cat /var/lib/rancher/k3s/server/node-token
-curl -v -u 'master:Admin2580!' -T /var/lib/rancher/k3s/server/node-token http://192.168.0.10:5005/WEBDAV/recv/token.log
+# 모든 워커 노드에 레이블을 설정하는 스크립트
+# 노드 목록을 가져와서 워커 노드에 레이블을 설정
+for NODE in $(kubectl get nodes --no-headers | grep "master\|control-plane" | awk '{print $1}')
+do
+  kubectl label node $NODE node-role.kubernetes.io/worker=worker --overwrite
+  echo "노드 $NODE에 'node-role.kubernetes.io/worker=worker' 레이블이 설정되었습니다."
+done
+
+# label 확인 
+kubectl get nodes --show-labels
+
+# node 확인 
+kubectl get nodes
