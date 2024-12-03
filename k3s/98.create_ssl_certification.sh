7c7fd11c5c7cf086c7145c99d5313c4d40ca983a
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-12-03 09:35:30 +0900

    Create 98.create_ssl_certification.sh

---
+++ k3s/98.create_ssl_certification.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 인증서 정보 설정
+DOMAIN="itapi.io"
+DAYS_VALID=3650
+COUNTRY="KR"
+STATE="Gyeonggi-do"
+LOCALITY="Republic Itapi"
+ORGANIZATION="ITAPI"
+ORG_UNIT="ITAPI Company"
+EMAIL="your-email@gamil.com"
+
+# 키와 인증서 경로 설정
+KEY_PATH="${DOMAIN}.key"
+CERT_PATH="${DOMAIN}.crt"
+CSR_PATH="${DOMAIN}.csr"
+
+# 개인 키 생성
+openssl genpkey -algorithm RSA -out $KEY_PATH -pkeyopt rsa_keygen_bits:2048
+
+# CSR (Certificate Signing Request) 생성
+openssl req -new -key $KEY_PATH -out $CSR_PATH -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION/OU=$ORG_UNIT/CN=$DOMAIN/emailAddress=$EMAIL"
+
+# 인증서 생성
+openssl x509 -req -days $DAYS_VALID -in $CSR_PATH -signkey $KEY_PATH -out $CERT_PATH
+
+# PEM 형식으로 변환
+cat $CERT_PATH $KEY_PATH > "${DOMAIN}.pem"
+
+# 생성된 파일 확인
+echo "Generated files:"
+echo "Private Key: $KEY_PATH"
+echo "Certificate: $CERT_PATH"
+echo "PEM File: ${DOMAIN}.pem"
