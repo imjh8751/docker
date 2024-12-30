3607e6089a90c4bcef2eb47b7fc7dd97f43d8f2c
jhlim <61223379+imjh8751@users.noreply.github.com>
2024-12-30 12:13:16 +0900

    Update install_nfs.sh

--- install_nfs.sh
+++ install_nfs.sh
@@ -1,4 +1,4 @@
-리눅스에서 특정 폴더를 NFS(Network File System) 공유 폴더로 설정하는 방법을 안내해드릴게요. 다음 단계를 따라 설정할 수 있습니다:
+# 리눅스에서 특정 폴더를 NFS(Network File System) 공유 폴더로 설정하는 방법을 안내해드릴게요. 다음 단계를 따라 설정할 수 있습니다:
 
 ### 1. NFS 서버 패키지 설치
 # 먼저 NFS 서버 패키지를 설치합니다:
@@ -7,10 +7,10 @@ sudo apt install -y nfs-kernel-server
 
 ### 2. 공유 폴더 설정
 # `/etc/exports` 파일을 편집하여 공유 폴더를 설정합니다:
-vi /etc/exports
+# vi /etc/exports
 
 # 파일에 다음 내용을 추가합니다:
-/volume2/DOCKER 192.168.0.0/24(rw,sync,no_subtree_check)
+# /volume2/DOCKER 192.168.0.0/24(rw,sync,no_subtree_check)
 
 # 이 예시에서는 `192.168.1.0/24` 네트워크에 있는 클라이언트가 폴더 `/path/to/share`를 읽고 쓸 수 있도록 설정합니다.
 
