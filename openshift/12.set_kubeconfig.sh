2b7a94284daf2061b357c3249ab95f23004cbdb7
jhlim <61223379+imjh8751@users.noreply.github.com>
2025-01-27 15:49:19 +0900

    Update 12.set_kubeconfig.sh

--- openshift/12.set_kubeconfig.sh
+++ openshift/12.set_kubeconfig.sh
@@ -5,3 +5,15 @@ systemctl restart haproxy
 echo "export KUBECONFIG=/root/installation_directory/auth/kubeconfig" >> ~/.bash_profile
 export KUBECONFIG=/root/installation_directory/auth/kubeconfig
 . /root/.bash_profile
+
+# bash completion code file 생성
+oc completion bash > oc_bash_completion
+
+# /etc/bash_completion.d/에 파일 복사
+cp oc_bash_completion /etc/bash_completion.d/
+
+# 또 다른 방법
+#oc completion bash > oc_completion.sh
+
+# .bashrc 마지막에 아래 내용 추가(파일 저장 위치 확인 필요)
+# source oc_completion.sh
