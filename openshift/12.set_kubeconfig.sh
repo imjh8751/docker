d886c63df9d704615260182a5278707c9380fddf
jhlim <61223379+imjh8751@users.noreply.github.com>
2025-01-27 13:18:07 +0900

    Update 12.set_kubeconfig.sh

--- openshift/12.set_kubeconfig.sh
+++ openshift/12.set_kubeconfig.sh
@@ -4,3 +4,4 @@ sed -i '/bootstrap/s/^/#/' /etc/haproxy/haproxy.cfg
 systemctl restart haproxy
 echo "export KUBECONFIG=/root/installation_directory/auth/kubeconfig" >> ~/.bash_profile
 export KUBECONFIG=/root/installation_directory/auth/kubeconfig
+. /root/.bash_profile
