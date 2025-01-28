1fd855d060f3e8a93453468cec2a61a8aecffa58
jhlim <61223379+imjh8751@users.noreply.github.com>
2025-01-28 22:35:01 +0900

    Update 4.set_named.conf.sh

--- openshift/4.set_named.conf.sh
+++ openshift/4.set_named.conf.sh
@@ -12,7 +12,7 @@ sed -i 's/listen-on port 53 {[^}]*};/listen-on port 53 { any; };/g' $NAMED_CONF
 sed -i 's/allow-query     {[^}]*};/allow-query     { any; };/g' $NAMED_CONF
 
 # forwarders 옵션 (탭 들여쓰기 포함)
-FORWARDERS="        forwarders { 8.8.8.8; 168.126.63.1; };"
+FORWARDERS="       forwarders { 8.8.8.8; 168.126.63.1; };"
 
 # sed 명령어를 사용하여 recursion yes; 아래에 들여쓰기된 forwarders 추가
 sed -i '/recursion yes;/a\ '"$FORWARDERS" "$NAMED_CONF"
