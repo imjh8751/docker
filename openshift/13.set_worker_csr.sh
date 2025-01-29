789d1e48c70f4dbc837d0fb7656ab41d90d41416
jhlim <61223379+imjh8751@users.noreply.github.com>
2025-01-29 22:04:40 +0900

    Update 13.set_worker_csr.sh

--- openshift/13.set_worker_csr.sh
+++ openshift/13.set_worker_csr.sh
@@ -3,7 +3,8 @@
 # Pending 상태의 node-bootstrapper CSR을 찾고 승인하는 스크립트
 
 # Pending CSR 목록 추출
-pending_csrs=$(oc get csr | grep Pending | grep node-bootstrapper | awk '{print $1}')
+#pending_csrs=$(oc get csr | grep Pending | grep node-bootstrapper | awk '{print $1}')
+pending_csrs=$(oc get csr | grep Pending | awk '{print $1}')
 
 # CSR 승인
 for csr in $pending_csrs; do
