edf9a043c829dfa92f1218940cbc86f489fdba96
jhlim <61223379+imjh8751@users.noreply.github.com>
2025-02-06 12:59:21 +0900

    Update install.sh

--- docker/REMOTE/guacamole/install.sh
+++ docker/REMOTE/guacamole/install.sh
@@ -1,4 +1,17 @@
-git clone "https://github.com/boschkundendienst/guacamole-docker-compose.git"
-cd guacamole-docker-compose
-./prepare.sh
-docker-compose up -d
+#!/bin/sh
+#
+# check if docker is running
+if ! (docker ps >/dev/null 2>&1)
+then
+        echo "docker daemon not running, will exit here!"
+        exit
+fi
+echo "Preparing folder init and creating ./init/initdb.sql"
+mkdir /APP/guacamole/init >/dev/null 2>&1
+chmod -R +x /APP/guacamole/init
+docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > /APP/guacamole/init/initdb.sql
+echo "done"
+echo "Preparing folder record and set permissions"
+mkdir /APP/guacamole/record >/dev/null 2>&1
+chmod -R 777 /APP/guacamole/record
+echo "done"
