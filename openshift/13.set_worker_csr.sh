760865e3933b97c28ebf63c9b853c9aa0ca0192b
jhlim <61223379+imjh8751@users.noreply.github.com>
2025-02-28 15:04:44 +0900

    Update 13.set_worker_csr.sh

--- openshift/13.set_worker_csr.sh
+++ openshift/13.set_worker_csr.sh
@@ -11,3 +11,7 @@ for csr in $pending_csrs; do
   oc adm certificate approve $csr
   echo "Approved CSR: $csr"
 done
+
+# 인증서가 만료되어 bastion에서 oc 명령어 수행이 되지 않을 경우 갱신 방법
+#ssh core@master01 sudo export KUBECONFIG=/etc/kubernetes/static-pod-resources/kube-apiserver-certs/secrets/node-kubeconfigs/lb-int.kubeconfig
+#ssh core@master01 sudo oc get csr -o name | xargs oc adm certificate approve
