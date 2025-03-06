734cb595290bc94177dd590cac589729782eb235
jhlim <61223379+imjh8751@users.noreply.github.com>
2025-03-06 16:49:50 +0900

    Update setup.sh

--- docker-images/image-nginx-web/setup.sh
+++ docker-images/image-nginx-web/setup.sh
@@ -2,6 +2,8 @@
 
 # Dockerfile로 이미지 빌드
 docker build -t nginx-app .
+#podman build -t nginx-app .
 
 # 컨테이너 실행
 docker run -d -p 8180:80 --name nginx-app nginx-app
+#podman run -d -p 8180:80 --name nginx-app nginx-app
