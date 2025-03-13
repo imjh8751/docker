24bafa4895061502932cdc3cd97e45a2d332f88e
jhlim <61223379+imjh8751@users.noreply.github.com>
2025-03-13 10:42:04 +0900

    Create build.sh

---
+++ docker/SECURITY/arachni/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://github.com/Arachni/arachni/releases/download/
+
+# 방법 1 : 변수 지
+docker build --build-arg VERSION=1.6.1.3 --build-arg WEB_VERSION=0.6.1.1 -t arachni .
+
+# 방법 2 : 기본 값
+# docker-compose build
+
+# 방법 3 : docker-compose 수행
+docker-compose up -d web
